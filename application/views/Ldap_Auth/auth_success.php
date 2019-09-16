eeb24b3bc3a0b6641c793d28642af0bb1578584c
Lucas Halbert <lhalbert@halbert.xyz>
2019-09-16 12:53:00 -0400

    v0.0.1 commit

---
+++ application/views/Ldap_Auth/auth_success.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>Authentication Success</title>
+    </head>
+    <body>
+        <h1>Authentication Success</h1>
+        <p>User login successful</p>
+        <p>To logout, navigate to <a href="/index.php/auth/logout"/>logout</p>
+    </body>
+</html>
