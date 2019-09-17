16358ee63ea26799fc9e3723481696404c7d8a3d
lucashalbert <lhalbert@lhalbert.xyz>
2019-09-17 14:27:43 -0400

    Fix link closure

--- application/views/Ldap_Auth/auth_success.php
+++ application/views/Ldap_Auth/auth_success.php
@@ -5,6 +5,6 @@
     <body>
         <h1>Authentication Success</h1>
         <p>User login successful</p>
-        <p>To logout, navigate to <a href="/index.php/auth/logout"/>logout</p>
+        <p>To logout, navigate to <a href="/index.php/auth/logout"/>logout</a></p>
     </body>
 </html>
